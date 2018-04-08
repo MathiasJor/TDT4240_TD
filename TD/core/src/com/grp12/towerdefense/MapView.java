2ab67cfad7b0c53ca40dad13f7ad4b7d63caafcd
torbjoks <torbjoks@stud.ntnu.no>
2018-04-08 14:21:33 +0100

    MapView class to draw the map tiles and its towers

---
+++ TD/core/src/com/grp12/towerdefense/MapView.java
@@ -0,0 +1,16 @@
+package com.grp12.towerdefense;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MapView {
+
+    private Map map;
+
+    public MapView(Map map) { this.map = map; }
+
+    public void drawMap(SpriteBatch sb) {
+        // draws the stuff
+    }
+
+
+}

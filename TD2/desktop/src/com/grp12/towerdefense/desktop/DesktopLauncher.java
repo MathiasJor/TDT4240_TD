97f70d751b461c124d94f68f1f6b010a37225ecc
Mathias Jørnsen <pajornse@stud.ntnu.no>
2018-04-08 12:57:19 +0200

    basic project v2

---
+++ TD2/desktop/src/com/grp12/towerdefense/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.grp12.towerdefense.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.grp12.towerdefense.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}

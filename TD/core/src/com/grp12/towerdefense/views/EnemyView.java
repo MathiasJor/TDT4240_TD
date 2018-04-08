2d48d1feaf52b2c8ec0d6b143971906fe44f5ef9
torbjoks <torbjoks@stud.ntnu.no>
2018-04-08 14:26:00 +0100

    Added EnemyView and folder for views

---
+++ TD/core/src/com/grp12/towerdefense/views/EnemyView.java
@@ -0,0 +1,10 @@
+package com.grp12.towerdefense.views;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class EnemyView {
+
+    public void draw(SpriteBatch sb) {
+        // draws the enemies on the screen
+    }
+}

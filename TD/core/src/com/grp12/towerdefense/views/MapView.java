6b6ce5a5683a2a1dcf2968f4303540092b718303
torbjoks <torbjoks@stud.ntnu.no>
2018-04-08 15:08:49 +0100

    MapView skeleton

--- TD/core/src/com/grp12/towerdefense/views/MapView.java
+++ TD/core/src/com/grp12/towerdefense/views/MapView.java
@@ -1,16 +1,30 @@
 package com.grp12.towerdefense.views;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.grp12.towerdefense.gamelogic.Map;
 
 public class MapView {
 
-    private Map map;
+    private int[][] grid;
+    private Texture path;
+    private Texture land;
 
-    public MapView(Map map) { this.map = map; }
+    public MapView(int[][] grid) {
+        this.grid = grid;
+        path = new Texture("towerDefense_tile024.png");
+        land = new Texture("towerDefense_tile050.png");
+    }
 
     public void draw(SpriteBatch sb) {
-        // draws the stuff
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                //Finn ut hvilken type node det er
+                //Tegn riktig Texture i coords i*img.height j*img.width
+                //sb.draw(path, x, y);
+            }
+        }
+
     }
 
 

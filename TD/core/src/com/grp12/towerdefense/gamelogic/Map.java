bb2c13a82afd16a571e11ff163c26abe27e48c0f
Kristian Stamland <kristiansta@hotmail.com>
2018-04-08 15:45:49 +0200

    Began implementing a map generation function

--- TD/core/src/com/grp12/towerdefense/gamelogic/Map.java
+++ TD/core/src/com/grp12/towerdefense/gamelogic/Map.java
@@ -1,5 +1,6 @@
 package com.grp12.towerdefense.gamelogic;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Map {
@@ -14,10 +15,53 @@ public class Map {
 
     public Map(){
         grid = new Node[height][width];
+        waypoints = new ArrayList<Node>();
+        generateMap(
+                "oosooooooooooooo\n" +
+                "ooxooooooooooooo\n" +
+                "ooxooooooooooooo\n" +
+                "ooxxxxxxxxxxoooo\n" +
+                "oooooooooooxoooo\n" +
+                "oooooooooooxoooo\n" +
+                "oooooooooooxoooo\n" +
+                "ooxxxxxxxxxxoooo\n" +
+                "ooxooooooooooooo\n" +
+                "ooxxxxxxxxoooooo\n" +
+                "oooooooooxoooooo\n" +
+                "oooooooooxoooooo\n" +
+                "oooooooooxxxxooo\n" +
+                "ooooooooooooxooo\n" +
+                "oooooooxxxxxxooo\n" +
+                "oooooooxoooooooo\n" +
+                "oooooooxoooooooo\n" +
+                "oooooooxxxxxxooo\n" +
+                "ooooooooooooxooo\n" +
+                "ooooooooooooeooo");
     }
 
-    public void generateMap(){
-        
+    public void generateMap(String mapString){
+        System.out.println(mapString);
+        String[] rows = mapString.split("\n");
+
+        for(int y = 0; y < rows.length; y++){
+            String row = rows[y];
+            for(int x = 0; x < row.length(); x++){
+                switch(row.charAt(x)){
+                    case 'o':
+                        grid[y][x] = new Node(Node.NodeType.TOWERNODE, false, x, y);
+                        break;
+                    case 'x':
+                        grid[y][x] = new Node(Node.NodeType.PATHNODE, false, x, y);
+                        break;
+                    default:
+                        Node waypoint = new Node(Node.NodeType.PATHNODE, true, x, y);
+                        waypoints.add(waypoint);
+                        break;
+                }
+            }
+        }
+
+
     }
 
     public Node[][] getGrid() {

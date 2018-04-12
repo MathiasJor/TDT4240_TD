64d973f663ddb60a8a822d644a5d430dae338059
torbjoks <torbjoks@stud.ntnu.no>
2018-04-12 14:03:33 +0100

    proper drawing in master branch

---
+++ TD/core/src/com/grp12/towerdefense/views/TowerView.java
@@ -0,0 +1,20 @@
+package com.grp12.towerdefense.views;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.grp12.towerdefense.gamelogic.towers.AbstractTower;
+
+import java.util.ArrayList;
+
+public class TowerView {
+
+    private ArrayList<AbstractTower> towers;
+
+    public TowerView() {
+        towers = new ArrayList<AbstractTower>();
+    }
+
+    public void draw(SpriteBatch sb) {
+
+        }
+
+}

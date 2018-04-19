ba37eb94c73777ca015ca5630604713da897d789
Kristian Stamland <krsta@stud.ntnu.no>
2018-04-19 23:56:38 +0200

    Simple Client changed to test out end turn functionality. Seems to work...

---
+++ TD/core/src/com/grp12/towerdefense/Network/NetworkGame.java
@@ -0,0 +1,13 @@
+package com.grp12.towerdefense.Network;
+
+import com.badlogic.gdx.utils.Json;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+/**
+ * Created by Kristian on 4/19/2018.
+ */
+
+public class NetworkGame {
+    Json json = new Json();
+}

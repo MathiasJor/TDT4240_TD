ef45c122c0cfe213a1e1eb138c2da25988cfcb08
Kristian Stamland <krsta@stud.ntnu.no>
2018-04-21 05:01:16 +0200

    Added messaging from client to server, need to implement handling of JSON responses. Added Google GSON library to build.gradle to use for JSON parsing

--- TD/core/src/com/grp12/towerdefense/Network/NetworkCommunicator.java
+++ TD/core/src/com/grp12/towerdefense/Network/NetworkCommunicator.java
@@ -2,6 +2,7 @@ package com.grp12.towerdefense.Network;
 
 import com.badlogic.gdx.utils.async.AsyncExecutor;
 import com.badlogic.gdx.utils.async.AsyncTask;
+import com.grp12.towerdefense.gamelogic.PlayerStats;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -13,7 +14,7 @@ import java.net.Socket;
  */
 
 public class NetworkCommunicator {
-    public static int userId;
+    public static int userId = 0;
 
     String hostName = "localhost";
     int port = 9999;
@@ -21,18 +22,89 @@ public class NetworkCommunicator {
     public NetworkCommunicator(){
     }
 
-    public static int getUserId(){
+    public static void getExternalUserId(){
+        Thread executor = new Thread(){
+            public void run(){
+                try{
+
+                    Socket s = new Socket("localhost", 9999);
+                    PrintWriter out = new PrintWriter(s.getOutputStream());
+                    System.out.println("This worked!");
+
+                    out.write("{\"type\":\"connect\", \"userId\": \"null\"}");
+                    out.flush();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    String line;
+                    while((line = in.readLine()) != null){
+                        System.out.println(line);
+                    }
+                    out.close();
+                    in.close();
+                    s.close();
+                    NetworkCommunicator.getGames();
+                }catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+        };
+        executor.start();
+    }
+
+    public static void getGames(){
+        Thread executor = new Thread(){
+            public void run(){
+                try{
+
+                    Socket s = new Socket("localhost", 9999);
+                    PrintWriter out = new PrintWriter(s.getOutputStream());
+                    System.out.println("This worked!");
+
+                    out.write(java.lang.String.format("{\"type\":\"getGames\", \"userId\":%d}", NetworkCommunicator.userId));
+                    out.flush();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    String line;
+                    while((line = in.readLine()) != null){
+                        System.out.println(line);
+                    }
+                    out.close();
+                    in.close();
+                    s.close();
+
+                }catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+        };
+        executor.start();
+    }
+
+    public static void sendEndTurnMessage(int gameId, PlayerStats ps){
+        Thread executor = new EndTurnThread(gameId, ps);
+        executor.start();
+    }
+}
+
+class EndTurnThread extends Thread{
+    int gameId;
+    PlayerStats ps;
+
+    public EndTurnThread(int gameId, PlayerStats ps){
+        this.gameId = gameId;
+        this.ps = ps;
+    }
+    public void run(){
         try{
 
             Socket s = new Socket("localhost", 9999);
             PrintWriter out = new PrintWriter(s.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             System.out.println("This worked!");
 
-            out.write("{\"type\":\"connect\", \"userId\": \"null\"}");
+            //TODO: Add health to the string. (Not done due to health not being implemented on branch at the time)
+            out.write(java.lang.String.format("{\"type\":\"endTurn\", \"userId\":%d,  \"gameId\":%d,  \"userHealth\":%d,  \"userGold\":%d}", NetworkCommunicator.userId, gameId, 30, ps.getBalance()));
+            out.flush();
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             String line;
             while((line = in.readLine()) != null){
-                System.out.println("whelp?");
                 System.out.println(line);
             }
             out.close();
@@ -42,60 +114,5 @@ public class NetworkCommunicator {
         }catch(Exception e){
             System.out.println(e);
         }
-        return 0;
-    }
-
-    public String getGames(){
-        AsyncExecutor exec = new AsyncExecutor(2);
-        exec.submit(new GetGameTask());
-        return "";
-    }
-}
-
-
-class GetGameTask<String> implements AsyncTask<String> {
-
-    @Override
-    public String call() throws Exception {
-        try{
-
-            Socket s = new Socket("localhost", 9999);
-            PrintWriter out = new PrintWriter(s.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-
-            out.write(java.lang.String.format("{\"type\":\"getGames\", \"userId\":%d}", NetworkCommunicator.userId));
-
-            System.out.println();
-        }catch(Exception e){
-        }
-        return (String) "";
-    }
-}
-
-class GetUserIdTask implements AsyncTask<Integer> {
-
-    @Override
-    public Integer call() throws Exception {
-        try{
-
-            Socket s = new Socket("localhost", 9999);
-            PrintWriter out = new PrintWriter(s.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            System.out.println("This worked!");
-
-            out.write("{\"type\":\"connect\", \"userId\": \"null\"}");
-            String line;
-            while((line = in.readLine()) != null){
-                System.out.println("whelp?");
-                System.out.println(line);
-            }
-            out.close();
-            in.close();
-            s.close();
-
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        return 0;
     }
 }

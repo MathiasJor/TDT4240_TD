e544c51dd1fa10acabc7779696adde96174534fb
Kristian Stamland <krsta@stud.ntnu.no>
2018-04-21 23:11:32 +0200

    Added some logic functions for NetworkGame I also added an Active Game variable to the NetworkCommunicator to explicitly set which game is selected

--- TD/core/src/com/grp12/towerdefense/Network/NetworkUser.java
+++ TD/core/src/com/grp12/towerdefense/Network/NetworkUser.java
@@ -7,6 +7,24 @@ package com.grp12.towerdefense.Network;
 public class NetworkUser {
     int health;
     int gold;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public boolean isTurn() {
+        return isTurn;
+    }
+
     boolean isTurn;
+
+    public int getId() {
+        return id;
+    }
+
     int id;
 }
